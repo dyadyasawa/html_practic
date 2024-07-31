b77346cb77344e470edeca7886526eeb7c4a6bf2
dyadyasawa <dyadyasawa@gmail.com>
2024-07-31 21:33:40 +0300

    Настройки.

--- config/settings.py
+++ config/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles"
+    "django.contrib.staticfiles",
     
     "users",
 ]
@@ -83,8 +83,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("NAME"),
+        "USER": os.getenv("USER"),
+        "PASSWORD": os.getenv("PASSWORD"),
+        "HOST": os.getenv("HOST"),
+        "PORT": os.getenv("PORT"),
     }
 }
 
--- users/models.py
+++ users/models.py
@@ -1,3 +1,24 @@
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+NULLABLE = {"blank": True, "null": True}
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Почта")
+    phone = models.CharField(max_length=50, verbose_name="Телефон", **NULLABLE)
+    city = models.CharField(max_length=50, verbose_name="Город", **NULLABLE)
+    avatar = models.ImageField(upload_to="users/avatars", verbose_name="Аватар", **NULLABLE)
+    token = models.CharField(max_length=100, verbose_name="Сгенерированный пароль", **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return f"{self.email}"
