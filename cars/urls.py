de1f672934cdad24512a273d27d441470e6da3a0
dyadyasawa <dyadyasawa@gmail.com>
2024-10-13 15:48:22 +0300

    CRUD для модели Category.

--- cars/urls.py
+++ cars/urls.py
@@ -5,13 +5,18 @@ from django.urls import path
 from cars.apps import CarsConfig
 from cars.views import (
     StartPage,
+
     CarsListView,
     CarDetailView,
     CarCreateView,
     CarUpdateView,
     CarDeleteView,
+
     CategoryList,
     CarsCategoriesListView,
+    CategoryCreateView,
+    CategoryUpdateView,
+    CategoryDeleteView,
 )
 
 app_name = CarsConfig.name
@@ -20,13 +25,14 @@ urlpatterns = [
     path("", StartPage.as_view(), name="start-page"),
 
     path("list/", CarsListView.as_view(), name="cars-list"),
-
-    path("cars_categories_list/<int:pk>/", CarsCategoriesListView.as_view(), name="cars-categories-list"),
-
     path("detail/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
     path("create/", CarCreateView.as_view(), name="car-create"),
     path("update/<int:pk>/", CarUpdateView.as_view(), name="car-update"),
     path("delete/<int:pk>/", CarDeleteView.as_view(), name="car-delete"),
 
     path("category_list/", CategoryList.as_view(), name="category-list"),
+    path("cars_categories_list/<int:pk>/", CarsCategoriesListView.as_view(), name="cars-categories-list"),
+    path("category_create/", CategoryCreateView.as_view(), name="category-create"),
+    path("category_update/<int:pk>/", CategoryUpdateView.as_view(), name="category-update"),
+    path("category_delete/<int:pk>/", CategoryDeleteView.as_view(), name="category-delete"),
 ]
--- cars/views.py
+++ cars/views.py
@@ -4,16 +4,19 @@ from django.urls import reverse_lazy
 from rest_framework.views import APIView
 from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 
-from cars.forms import CarForm
+from cars.forms import CarForm, CategoryForm
 # from cars.paginations import CustomPagination
 # from cars.serializers import ProductSerializer
 from cars.models import Car, Category
 
+# Стартовая страница сайта
 
-# class StartPageAPIView(APIView):
+
 class StartPage(TemplateView):
     """ Стартовая страница. """
     template_name = "cars_app/start_page.html"
+
+# CRUD для модели Car
 
 
 class CarsListView(ListView):
@@ -21,24 +24,6 @@ class CarsListView(ListView):
 
     model = Car
     template_name = "cars_app/cars_list.html"
-
-
-class CarsCategoriesListView(ListView):
-    """ Выводим список машин определенной категории. """
-
-    model = Category
-    template_name = "cars_app/cars_list_categories.html"
-
-    def get_queryset(self, *args, **kwargs):
-
-        queryset = super().get_queryset(*args, **kwargs)
-        id_category = self.kwargs.get("pk")  # Получаем id категории из-под которой заходим
-
-        queryset = queryset.get(pk=id_category)  # Получаем единственную категорию по id
-
-        queryset = queryset.car_set.all()  # Формируем queryset из элементов модели Car с заданной категорией (по id)
-
-        return queryset
 
 
 class CarDetailView(DetailView):
@@ -87,9 +72,55 @@ class CarDeleteView(DeleteView):
     template_name = 'cars_app/car_confirm_delete.html'
     success_url = reverse_lazy('cars:cars-list')
 
+# CRUD для модели Category
+
 
 class CategoryList(ListView):
     """ Выводим список категорий. """
 
     model = Category
     template_name = "cars_app/category_list.html"
+
+
+class CarsCategoriesListView(ListView):
+    """ Выводим список машин определенной категории. """
+
+    model = Category
+    template_name = "cars_app/cars_list_categories.html"
+
+    def get_queryset(self, *args, **kwargs):
+
+        queryset = super().get_queryset(*args, **kwargs)
+        id_category = self.kwargs.get("pk")  # Получаем id категории из-под которой заходим
+
+        queryset = queryset.get(pk=id_category)  # Получаем единственную категорию по id
+
+        queryset = queryset.car_set.all()  # Формируем queryset из элементов модели Car с заданной категорией (по id)
+
+        return queryset
+
+
+class CategoryCreateView(CreateView):
+    """ Создаем категорию. """
+
+    model = Category
+    template_name = 'cars_app/category_form.html'
+    form_class = CategoryForm
+    success_url = reverse_lazy('cars:category-list')
+
+
+class CategoryUpdateView(UpdateView):
+    """ Редактируем выбранную категорию. """
+
+    model = Category
+    template_name = 'cars_app/category_form.html'
+    form_class = CategoryForm
+    success_url = reverse_lazy('cars:category-list')
+
+
+class CategoryDeleteView(DeleteView):
+    """ Удаляем выбранную категорию. """
+
+    model = Category
+    template_name = 'cars_app/category_confirm_delete.html'
+    success_url = reverse_lazy('cars:category-list')
